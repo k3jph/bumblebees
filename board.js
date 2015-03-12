9190546dba60847fd5d065be2b21990f0a3e7ca8
James P. Howard, II <jh@jameshoward.us>
2015-03-11 22:20:22 -0400

    Changed base program to the generalized ants

--- board.js
+++ board.js
@@ -1,3 +1,12 @@
+var CLCELL = 0x00;
+var REDDOT = 0x10;
+var BLUDOT = 0x11;
+
+var UPANT = 0x20;
+var RTANT = 0x21;
+var DNANT = 0x22;
+var LTANT = 0x23;
+
 var util = util || {}
 
 util.fps = {
@@ -27,76 +36,73 @@ var ant = function(x, y, r, t) {
         type: t
     }
 }
+
 var anim = {
 
     interval: false,
 
     tick: function() {
-
         // detect any clicks
         if (this.clicks.length > 0) {
             for (var i in this.clicks) {
-
                 var x = Math.floor(this.clicks[i][0] / this.cellsize);
                 var y = Math.floor(this.clicks[i][1] / this.cellsize);
 
-                if (this.clicks[i][2] == 'ant') {
-                    this.putNewAnt(x, y, util.random(0, 3));
-                }
-
-                if (this.clicks[i][2] == 'block') {
-                    this.putBlock(x, y, 1);
-                }
-
-                if (this.clicks[i][2] == 'pat1') {
-                    this.pattern([x, y], 1);
-                }
-                if (this.clicks[i][2] == 'pat2') {
-                    this.pattern([x, y], 2);
-                }
-                if (this.clicks[i][2] == 'pat3') {
-                    this.pattern([x, y], 3);
-                }
-                if (this.clicks[i][2] == 'pat4') {
-                    this.pattern([x, y], 4);
-                }
-
+                if (this.clicks[i][2] == 'upant')
+                    this.putNewAnt(x, y, UPANT);
+				if (this.clicks[i][2] == 'rtant')
+                    this.putNewAnt(x, y, RTANT);
+				if (this.clicks[i][2] == 'dnant')
+                    this.putNewAnt(x, y, DNANT);
+                if (this.clicks[i][2] == 'ltant')
+                    this.putNewAnt(x, y, LTANT);
+                if (this.clicks[i][2] == 'reddot')
+                    this.putBlock(x, y, REDDOT);
+				if (this.clicks[i][2] == 'bludot')
+                    this.putBlock(x, y, BLUDOT);
+				if (this.clicks[i][2] == 'clcell')
+                    this.putBlock(x, y, CLCELL);
             }
             this.clicks = [];
         }
 
         // save the current board so all ants act on same basis
-
         var board = this.board.slice();
         for (var i in this.ants) {
             this.update(board, this.ants[i]);
         }
 
         this.draw();
-
         this.counter++;
     },
 
-
+	drawDot: function(i, c) {
+		this.canvas.beginPath();
+		this.canvas.arc(this.todraw[i][0] * this.cellsize + this.cellsize / 2,
+						this.todraw[i][1] * this.cellsize + this.cellsize / 2,
+						this.cellsize * 1/4, 0, 2 * Math.PI, false);
+		this.canvas.fillStyle = c;
+		this.canvas.fill();
+		this.canvas.lineWidth = 0;
+		this.canvas.strokeStyle = c;
+		this.canvas.stroke();
+	},
 
     draw: function() {
 
         for (var i in this.todraw) {
+			this.canvas.fillStyle = "black";
+			this.canvas.fillRect(this.todraw[i][0] * this.cellsize,
+								 this.todraw[i][1] * this.cellsize,
+								 this.cellsize, this.cellsize);
 
-            var colour = false;
+			if(this.todraw[i][2] == REDDOT)
+				this.drawDot(i, "red");
+			if(this.todraw[i][2] == BLUDOT)
+				this.drawDot(i, "blue");
 
-            if (this.todraw[i][2] == 0) { // empty
-                colour = "black";
-            } else if (this.todraw[i][2] == 1) { // block
-                colour = "green";
-            } else if (this.todraw[i][2] == 2) { // red
-                colour = "red";
-            }
-
-            this.canvas.fillStyle = colour;
-            this.canvas.fillRect((this.todraw[i][0] * this.cellsize), (
-                    this.todraw[i][1] * this.cellsize), this.cellsize,
-                this.cellsize);
+			if(this.todraw[i][2] >= UPANT)
+				this.drawDot(i, "white");
         }
 
         this.todraw = [];
@@ -106,17 +112,13 @@ var anim = {
     },
 
     putBlock: function(x, y, c) {
-
         this.board[y][x] = c;
         this.todraw.push([x, y, c]);
     },
 
     putNewAnt: function(x, y, d) {
-
         this.ants.push(ant(x, y, d));
-
         this.todraw.push([x, y, 2]);
-
     },
 
 
@@ -124,40 +126,28 @@ var anim = {
 
         if (!this.running) return;
 
-        /*
-		At a black square, turn 90° right, flip the color of the square, move forward one unit
-		At a green square, turn 90° left, flip the color of the square, move forward one unit
-		*/
-
-        // what colour cell am i on?
-        if (board[ant.currY][ant.currX] == 0) {
-
-            // new square is off, turn 90 right
-            ant.currR = (ant.currR == 3) ? 0 : (ant.currR + 1);
-
-            // flip cell colour
-            this.putBlock(ant.currX, ant.currY, 1);
-
+		//  On a red dot, turn right.  On a blue dot, turn left.
+        if (board[ant.currY][ant.currX] == REDDOT) {
+            ant.currR = (ant.currR == LTANT) ? UPANT : (ant.currR + 1);
+            this.putBlock(ant.currX, ant.currY, BLUDOT);
+        } else if (board[ant.currY][ant.currX] == BLUDOT) {
+            ant.currR = (ant.currR == UPANT) ? LTANT : (ant.currR - 1);
+            this.putBlock(ant.currX, ant.currY, REDDOT);
         } else {
-
-            // new square is off, turn 90 right
-            ant.currR = (ant.currR == 0) ? 3 : (ant.currR - 1);
-
-            // flip cell colour
-            this.putBlock(ant.currX, ant.currY, 0);
-        }
+			this.putBlock(ant.currX, ant.currY, CLCELL);
+		}
 
         // 2 advance in the direction i should move
-        if (ant.currR == 0) {
+        if (ant.currR == UPANT) {
             ant.currY--;
         }
-        if (ant.currR == 1) {
+        if (ant.currR == RTANT) {
             ant.currX++;
         }
-        if (ant.currR == 2) {
+        if (ant.currR == DNANT) {
             ant.currY++;
         }
-        if (ant.currR == 3) {
+        if (ant.currR == LTANT) {
             ant.currX--;
         }
 
@@ -175,9 +165,8 @@ var anim = {
             ant.currY = (this.rows - 1);
         }
 
-
         // draw ant
-        this.todraw.push([ant.currX, ant.currY, 2]);
+        this.todraw.push([ant.currX, ant.currY, ant.currR]);
 
     },
 
@@ -235,7 +224,6 @@ var anim = {
     },
 
     start: function() {
-
         this.running = true;
 
         this.stop();
@@ -252,76 +240,7 @@ var anim = {
 
     stopRunning: function() {
         this.running = false;
-    },
-
-    pattern: function(cells, num) {
-
-        if (num == 1) {
-
-            // phaser
-
-            this.putNewAnt(cells[0], cells[1], 1);
-            this.putNewAnt(cells[0], cells[1] + 1, 3);
-        }
-
-        if (num == 2) {
-
-            // growing square
-
-            this.putNewAnt(cells[0], cells[1], 0);
-            this.putNewAnt(cells[0], cells[1] + 1, 2);
-        }
-
-        if (num == 3) {
-
-            // in a line
-
-            this.putBlock(cells[0], cells[1], 1);
-            this.putBlock(cells[0] + 1, cells[1], 1);
-            this.putBlock(cells[0] + 2, cells[1], 1);
-            this.putBlock(cells[0] + 3, cells[1], 1);
-            this.putBlock(cells[0] + 4, cells[1], 1);
-            this.putBlock(cells[0] + 5, cells[1], 1);
-            this.putBlock(cells[0] + 6, cells[1], 1);
-            this.putBlock(cells[0] + 7, cells[1], 1);
-            this.putBlock(cells[0] + 8, cells[1], 1);
-            this.putBlock(cells[0] + 9, cells[1], 1);
-            this.putBlock(cells[0] + 10, cells[1], 1);
-            this.putBlock(cells[0] + 11, cells[1], 1);
-            this.putBlock(cells[0] + 12, cells[1], 1);
-
-            this.putNewAnt(cells[0] + 12, cells[1], 3);
-
-        }
-
-        if (num == 4) {
-
-            // walkies
-            //this.putBlock(cells[0], 	cells[1]-1, 1);
-
-            this.putBlock(cells[0] + 1, cells[1], 1);
-
-            this.putBlock(cells[0], cells[1] + 1, 1);
-            this.putBlock(cells[0] + 1, cells[1] + 1, 1);
-
-            this.putNewAnt(cells[0], cells[1] - 1, 1);
-            this.putNewAnt(cells[0], cells[1] - 1, 2);
-        }
-
-        if (num == 5) {
-
-            // phaser 2
-            this.putBlock(cells[0] + 1, cells[1], 1);
-
-            this.putBlock(cells[0], cells[1] + 1, 1);
-            this.putBlock(cells[0] + 1, cells[1] + 1, 1);
-
-            this.putNewAnt(cells[0], cells[1], 3);
-            this.putNewAnt(cells[0] + 1, cells[1] - 1, 0);
-        }
-
     }
-
 }
 
 $(document)
